1c6416f0182bad009abbf87ed29b6ab4ec1728d2
theguardianking <yogaastri0902@gmail.com>
2022-10-29 16:16:22 +0700

    Frontend Blog Show Setup Part 3

--- app/Http/Controllers/Home/BlogController.php
+++ app/Http/Controllers/Home/BlogController.php
@@ -102,4 +102,11 @@ class BlogController extends Controller
 
         return redirect()->back()->with($notification);
     } // end method
+
+    public function BlogDetails($id){
+        $allblogs = Blog::latest()->limit(5)->get();
+        $blogs = Blog::findOrFail($id);
+        $categories = BlogCategory::orderBy('blog_category','ASC')->get();
+        return view('frontend.blog_details',compact('blogs','allblogs','categories'));
+    } // end method
 }
--- resources/views/frontend/home_all/home_blog.blade.php
+++ resources/views/frontend/home_all/home_blog.blade.php
@@ -1,51 +1,29 @@
+@php
+$blogs = App\Models\Blog::latest()->limit(3)->get();
+@endphp
+
 <section class="blog">
 <div class="container">
 <div class="row gx-0 justify-content-center">
+
+    @foreach($blogs as $item)
     <div class="col-lg-4 col-md-6 col-sm-9">
         <div class="blog__post__item">
             <div class="blog__post__thumb">
-                <a href="blog-details.html"><img src="assets/img/blog/blog_post_thumb01.jpg" alt=""></a>
+                <a href="{{ route('blog.details',$item->id) }}"><img src="{{ asset($item->blog_image) }}" alt=""></a>
                 <div class="blog__post__tags">
-                    <a href="blog.html">Story</a>
+                    <a href="{{ route('blog.details',$item->id) }}">{{ $item['category']['blog_category'] }}</a>
                 </div>
             </div>
             <div class="blog__post__content">
-                <span class="date">13 january 2021</span>
-                <h3 class="title"><a href="blog-details.html">Facebook design is dedicated to what's new in design</a></h3>
-                <a href="blog-details.html" class="read__more">Read mORe</a>
+                <span class="date">{{ Carbon\Carbon::parse($item->created_at)->diffForHumans() }}</span>
+                <h3 class="title"><a href="{{ route('blog.details',$item->id) }}">{{ $item->blog_title }}</a></h3>
+                <a href="{{ route('blog.details',$item->id) }}" class="read__more">Read mORe</a>
             </div>
         </div>
     </div>
-    <div class="col-lg-4 col-md-6 col-sm-9">
-        <div class="blog__post__item">
-            <div class="blog__post__thumb">
-                <a href="blog-details.html"><img src="assets/img/blog/blog_post_thumb02.jpg" alt=""></a>
-                <div class="blog__post__tags">
-                    <a href="blog.html">Social</a>
-                </div>
-            </div>
-            <div class="blog__post__content">
-                <span class="date">13 january 2021</span>
-                <h3 class="title"><a href="blog-details.html">Make communication Fast and Effectively.</a></h3>
-                <a href="blog-details.html" class="read__more">Read mORe</a>
-            </div>
-        </div>
-    </div>
-    <div class="col-lg-4 col-md-6 col-sm-9">
-        <div class="blog__post__item">
-            <div class="blog__post__thumb">
-                <a href="blog-details.html"><img src="assets/img/blog/blog_post_thumb03.jpg" alt=""></a>
-                <div class="blog__post__tags">
-                    <a href="blog.html">Work</a>
-                </div>
-            </div>
-            <div class="blog__post__content">
-                <span class="date">13 january 2021</span>
-                <h3 class="title"><a href="blog-details.html">How to increase your productivity at work - 2021</a></h3>
-                <a href="blog-details.html" class="read__more">Read mORe</a>
-            </div>
-        </div>
-    </div>
+    @endforeach
+
 </div>
 <div class="blog__button text-center">
     <a href="blog.html" class="btn">more blog</a>
