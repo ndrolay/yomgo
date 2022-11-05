cef86f93242935a42996844940aa23ec90b181bd
theguardianking <yogaastri0902@gmail.com>
2022-11-06 03:42:17 +0700

    Middleware Groups in Route

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,8 @@ Route::controller(HomeController::class)->group(function () {
 });
 
 // Admin Page
+Route::middleware(['auth'])->group(function(){
+
 Route::controller(AdminController::class)->group(function () {
     Route::get('/admin/logout', 'destroy')->name('admin.logout');
     Route::get('/admin/profile', 'Profile')->name('admin.profile');
@@ -39,6 +41,7 @@ Route::controller(AdminController::class)->group(function () {
     Route::post('/store/profile', 'StoreProfile')->name('store.profile');
     Route::get('/change/password', 'ChangePassword')->name('change.password');
     Route::post('/update/password', 'UpdatePassword')->name('update.password');
+});
 });
 
 // Home Slide
@@ -49,30 +52,37 @@ Route::controller(HomeSliderController::class)->group(function () {
 
 // About Page
 Route::controller(AboutController::class)->group(function () {
+    Route::middleware(['auth'])->group(function(){
     Route::get('/about/page', 'AboutPage')->name('about.page');
     Route::post('/update/about', 'UpdateAbout')->name('update.about');
-    Route::get('/about', 'HomeAbout')->name('home.about');
     Route::get('/about/multi/image', 'AboutMultiImage')->name('about.multi.image');
-    Route::post('/store/multi/image', 'StoreMultiImage')->name('store.multi.image');
     Route::get('/all/multi/image', 'AllMultiImage')->name('all.multi.image');
+    Route::post('/store/multi/image', 'StoreMultiImage')->name('store.multi.image');
     Route::get('/edit/multi/image/{id}', 'EditMultiImage')->name('edit.multi.image');
     Route::post('/update/multi/image', 'UpdateMultiImage')->name('update.multi.image');
     Route::get('/delete/multi/image/{id}', 'DeleteMultiImage')->name('delete.multi.image');
+    });
+    Route::get('/about', 'HomeAbout')->name('home.about');
 });
 
 // Portfolio
 Route::controller(PortfolioController::class)->group(function () {
+    Route::middleware(['auth'])->group(function(){
+
     Route::get('/all/portfolio', 'AllPortfolio')->name('all.portfolio');
     Route::get('/add/portfolio', 'AddPortfolio')->name('add.portfolio');
     Route::post('/store/portfolio', 'StorePortfolio')->name('store.portfolio');
     Route::get('/edit/portfolio/{id}', 'EditPortfolio')->name('edit.portfolio');
     Route::post('/update/portfolio', 'UpdatePortfolio')->name('update.portfolio');
     Route::get('/delete/portfolio/{id}', 'DeletePortfolio')->name('delete.portfolio');
+    });
     Route::get('/portfolio/details/{id}', 'PortfolioDetails')->name('portfolio.details');
     Route::get('/portfolio', 'HomePortfolio')->name('home.portfolio');
 });
 
 // Blog Category
+Route::middleware(['auth'])->group(function(){
+
 Route::controller(BlogCategoryController::class)->group(function () {
     Route::get('/all/blog/category', 'AllBlogCategory')->name('all.blog.category');
     Route::get('/add/blog/category', 'AddBlogCategory')->name('add.blog.category');
@@ -81,15 +91,19 @@ Route::controller(BlogCategoryController::class)->group(function () {
     Route::post('/update/blog/category/{id}', 'UpdateBlogCategory')->name('update.blog.category');
     Route::get('/delete/blog/category/{id}', 'DeleteBlogCategory')->name('delete.blog.category');
 });
+});
 
 // Blog Category
 Route::controller(BlogController::class)->group(function () {
+    Route::middleware(['auth'])->group(function(){
+
     Route::get('/all/blog', 'AllBlog')->name('all.blog');
     Route::get('/add/blog', 'AddBlog')->name('add.blog');
     Route::post('/store/blog', 'StoreBlog')->name('store.blog');
     Route::get('/edit/blog/{id}', 'EditBlog')->name('edit.blog');
     Route::post('/update/blog', 'UpdateBlog')->name('update.blog');
     Route::get('/delete/blog/{id}', 'DeleteBlog')->name('delete.blog');
+    });
     Route::get('/blog/details/{id}', 'BlogDetails')->name('blog.details');
     Route::get('/category/blog/{id}', 'CategoryBlog')->name('category.blog');
     Route::get('/blog', 'HomeBlog')->name('home.blog');
@@ -97,7 +111,9 @@ Route::controller(BlogController::class)->group(function () {
 
 // Footer
 Route::controller(FooterController::class)->group(function () {
+    Route::middleware(['auth'])->group(function(){
     Route::get('/footer/setup', 'FooterSetup')->name('footer.setup');
+    });
     Route::post('/update/footer', 'UpdateFooter')->name('update.footer');
 });
 
@@ -105,8 +121,11 @@ Route::controller(FooterController::class)->group(function () {
 Route::controller(ContactController::class)->group(function () {
     Route::get('/contact', 'Contact')->name('contact.me');
     Route::post('/store/message', 'StoreMessage')->name('store.message');
+
+    Route::middleware(['auth'])->group(function(){
     Route::get('/contact/message', 'ContactMessage')->name('contact.message');
     Route::get('/delete/message/{id}', 'DeleteMessage')->name('delete.message');
+    });
 });
 
 Route::get('/dashboard', function () {
